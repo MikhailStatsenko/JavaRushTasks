f8db209bd024205f582371d871c90c0ebb806388
mikhail statsenko <statsenko.mikhail.a@gmail.com>
2023-08-06 17:12:28 +0300

    OOP base pt 2

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int val) {
+
+    }
+    public static void print(long val) {
+
+    }
+    public static void print(String val) {
+
+    }
+    public static void print(boolean val) {
+
+    }
+    public static void print(short val) {
+
+    }
+
+}
